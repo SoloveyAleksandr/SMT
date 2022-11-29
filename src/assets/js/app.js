72e6bdb8121f246cd98f04b9b5d20da83ae63f99
unknown <solalex.s.a@gmail.com>
2022-11-29 20:45:21 +0300

    add main slider

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,34 @@
-class dropDown {
-  constructor(listElement, buttonElement) {
-    this.list = typeof listElement === "string" ? document.querySelector(listElement) : listElement
-    this.button = typeof buttonElement === "string" ? document.querySelector(buttonElement) : buttonElement
-    this._init()
-  }
+// class dropDown {
+//   constructor(listElement, buttonElement) {
+//     this.list = typeof listElement === "string" ? document.querySelector(listElement) : listElement
+//     this.button = typeof buttonElement === "string" ? document.querySelector(buttonElement) : buttonElement
+//     this._init()
+//   }
 
-  _init() {
-    this.button.addEventListener('click', this.toggleMenu.bind(this))
-  }
+//   _init() {
+//     this.button.addEventListener('click', this.toggleMenu.bind(this))
+//   }
 
-  toggleMenu() {
-    this.list.classList.toggle('_open')
-    this.button.classList.toggle('_open')
-  }
-}
+//   toggleMenu() {
+//     this.list.classList.toggle('_open')
+//     this.button.classList.toggle('_open')
+//   }
+// }
+
+const mainSwiper = new Swiper('.main-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  effect: 'fade',
+  speed: 500,
+
+  pagination: {
+    el: '.main-swiper-pagination',
+    type: 'fraction',
+  },
+
+  navigation: {
+    nextEl: '.main-swiper-btn',
+  },
+
+  scrollbar: false,
+});
