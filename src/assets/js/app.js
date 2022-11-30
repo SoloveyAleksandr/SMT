6e0bdc593c9f6208ef4c2784c78a0a5862cd9c87
hyper-design <hyper-design@keramin.int>
2022-11-30 19:39:46 +0300

    add samples

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,6 +20,33 @@ const relax = new Rellax('.rellax', {
   center: true
 });
 
+const samplesSwiperPagination = document.querySelector('.samples-list');
+const samplesSwiper = new Swiper('.samples-swiper', {
+  direction: 'vertical',
+  loop: false,
+  effect: 'slide',
+  speed: 500,
+  mousewheel: {
+    releaseOnEdges: true
+  },
+
+  pagination: {
+    el: '.samples-list',
+    type: 'custom',
+    renderCustom: function (swiper, current, total) {
+      [...samplesSwiperPagination.children].forEach((item, id) => {
+        item.classList.remove('_next');
+        if (id !== current - 1) {
+          item.classList.remove('_active');
+        } else {
+          item.classList.add('_active');
+        }
+        id === current && item.classList.add('_next');
+      })
+    }
+  }
+});
+
 const headerMenuBtn = document.querySelector('.header-menu-btn');
 headerMenuBtn.onclick = () => {
   headerMenuBtn.classList.toggle('_active');
